577e756bc63dca3f527ca1765364670cd5d990a7
Shellie Who <s665550@sbuniv.edu>
2017-04-26 19:46:52 -0500

    Creating a new loginform using redux-thunk

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+ componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyDxVXeoctWPuGj6HlhAWwAJdd5PRDt98LY',
+            authDomain: 'authentication-9f27e.firebaseapp.com',
+            databaseURL: 'https://authentication-9f27e.firebaseio.com',
+            storageBucket: 'authentication-9f27e.appspot.com',
+            messagingSenderId: '744279226808'
+        };
+  firebase.initializeApp(config);
+  }
+
+    render() {
+        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+        return (
+            <Provider store={store}>
+                <LoginForm />
+            </Provider>
+        );
+    }
+};
+
+export default App;
